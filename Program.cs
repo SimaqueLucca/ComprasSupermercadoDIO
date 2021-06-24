54684f6c703931c678aa97879686c0bd6cbdf79c
Luca Simaque Souza <luccasimaque@yahoo.com.br>
2021-06-24 19:15:35 -0300

    inserindo pastas

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ComprasSupermercado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var totalDeCasosDeTeste = int.Parse(Console.ReadLine());
+            List<string> listasDeCompras = new List<string>();
+
+            for (int i = 0; i < totalDeCasosDeTeste; i++)
+            {
+                string lista = Console.ReadLine();
+
+                var listaSemRepeticoes = lista.Split(" ").Distinct();
+
+                List<string> listaOrdenada = listaSemRepeticoes.ToList();
+                listaOrdenada.Sort();
+
+                string listaEmString = string.Join(" ", listaOrdenada.ToArray());
+
+                listasDeCompras.Add(listaEmString);
+            }
+            listasDeCompras.ForEach(Console.WriteLine);
+        }
+
+    }
+}
